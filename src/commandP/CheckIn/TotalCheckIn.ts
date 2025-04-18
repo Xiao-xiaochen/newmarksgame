9d8850431505c5e62245c0c12ca0bf720a209ec8
Xiaochen18217 <18217090930@163.com>
2025-04-18 21:58:56 +0800

    0.1.400

--- src/commandP/CheckIn/TotalCheckIn.ts
+++ src/commandP/CheckIn/TotalCheckIn.ts
@@ -1,6 +1,7 @@
 // src/commandP/CheckIn.ts
 
 import { Context, Schema } from 'koishi'
+import { userdata } from '../../types'
 
 export function CheckIn(ctx: Context) {
   ctx.command('阅读报告')
@@ -11,21 +12,44 @@ export function CheckIn(ctx: Context) {
         return '会话不存在'
       }
       try {
-        let Userinfo = await ctx.database.get('userdata', { userId })
-      }
+        let Userinfo = await ctx.database.get('userdata', { userId }) as userdata[]
+        if (!Userinfo || Userinfo.length === 0) {
+          const Newuser: userdata = {
+            userId: session?.userId,
+            hasCheckedIn: true,
+            population: 100,
+            base: 100,
+            Department: 100,
+            farms: 100,
+            food: 100
+          }
+          await ctx.database.create('userdata', Newuser);
+          Userinfo = [Newuser]
+          const userDATA = Userinfo[0]
           return `
+===[新马列文游]===
+欢迎游玩新马列文游
+${username}
+获得物资：
+■ 人口：${userDATA.population}
+■ 基础设施：${userDATA.base}
+■ 建筑部门：${userDATA.Department}
+■ 轻工厂：${userDATA.farms}
+■ 农田：${userDATA.food}
+■ 粮食：${userDATA.food}
+`.trim()
+        }
+      return `
 ===[新马列文游]===
 ${username}
 获得物资：
-■ 人口：未完成
-■ 基础设施：未完成
-■ 建筑部门：未完成
-■ 轻工厂：未完成
-■ 农田：未完成
-■ 粮食：未完成
+待完成
 `.trim()
     }
-  )
+    catch (error) {
+      return '发生错误'
+    }
+  })
 }
 
 
